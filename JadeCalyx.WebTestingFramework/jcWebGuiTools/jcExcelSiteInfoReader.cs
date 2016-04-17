c64b0bffe197e675230fe2c632a6cf80a08aeb84
JadeCalyx <jadecalyx@outlook.com>
2016-04-17 15:47:54 -0400

    Adding files for Excel data

---
+++ JadeCalyx.WebTestingFramework/jcWebGuiTools/jcExcelSiteInfoReader.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeOpenXml;
+using System.IO;
+using System.Reflection;
+
+namespace jcWebGuiTools
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class jcExcelSiteInfoReader
+    {
+        private Dictionary<string, List<List<string>>> _worksheets = new Dictionary<string, List<List<string>>>();
+
+        /// <summary>
+        /// Constructor. 
+        /// </summary>
+        /// <param name="wbPath">Path to the workbook to read.</param>
+        public jcExcelSiteInfoReader(string wbPath)
+        {
+            loadExcelWorkbook(wbPath);
+        }
+
+        /// <summary>
+        /// Returns the entire excel file stored as a dictionary.
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<string, List<List<string>>> GetWorksheets()
+        {
+            return _worksheets;
+        }
+
+        /// <summary>
+        /// Loads an excel xslx file into the _worksheets dictionary.
+        /// </summary>
+        /// <param name="wbPath"></param>
+        private void loadExcelWorkbook(string wbPath)
+        {
+            Directory.SetCurrentDirectory(System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            if (!File.Exists(wbPath))
+            {
+                throw new Exception(String.Format("Excel workbook file not found at: {0}",
+                    wbPath));
+            }
+            byte[] file = File.ReadAllBytes(wbPath);
+            var ms = new MemoryStream(file);
+            using (var package = new ExcelPackage(ms))
+            {
+                if (package.Workbook.Worksheets.Count == 0)
+                    Console.Write("*** Your Excel file does not contain any work sheets");
+                else
+                {
+                    foreach (ExcelWorksheet worksheet in package.Workbook.Worksheets)
+                    {
+                        var worksheetRows = new List<List<string>>();
+                        try
+                        {
+                            worksheet.Select(worksheet.Dimension.Address);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("*** Worksheet was empty");
+                            _worksheets.Add(worksheet.Name.ToLower(), worksheetRows);
+                            continue;
+                        }
+                        var range = worksheet.SelectedRange;
+
+                        var rowCount = range.Rows;
+                        var colCount = range.Columns;
+                        for (var i = 1; i <= rowCount; i++)
+                        {
+                            var currRow = processCellsInARow(range, i, colCount);
+                            if (currRow.Any())
+                            {
+                                worksheetRows.Add(currRow);
+                            }
+                        }
+                        _worksheets.Add(worksheet.Name.ToLower(), worksheetRows);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Read one row from worksheet.
+        /// If the first column is blank or "-" return an empty list.
+        /// Cells containing the string BLANK are returned as String.Empty;
+        /// </summary>
+        /// <param name="range"></param>
+        /// <param name="row"></param>
+        /// <param name="colCount"></param>
+        /// <returns>List<string> with each item in the row.</string></returns>
+        private List<string> processCellsInARow(ExcelRange range, int row, int colCount)
+        {
+            var returnRow = new List<string>();
+
+            for (var i = 1; i <= colCount; i++)
+            {
+                var currCell = range[row, i].Text.Trim();
+                if (i == 1 && (currCell == "-" | currCell == String.Empty))
+                {
+                    return returnRow;
+                }
+                if (currCell == "BLANK")
+                {
+                    currCell = String.Empty;
+                }
+                if (currCell.Equals(string.Empty))
+                {
+                    continue; //do not add blank cells
+                }
+                returnRow.Add(currCell);
+            }
+            return returnRow;
+        }
+    }
+}
+
---
+++ JadeCalyx.WebTestingFramework/jcWebGuiTools/jcPageObjectRepository.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jcWebGuiTools
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class jcPageObjectRepository
+    {
+        private Dictionary<string, List<List<string>>> _pagesInfo = new Dictionary<string, List<List<string>>>();
+
+        /// <summary>
+        /// Provides access to page object info.
+        /// Meant to eventually be flexible as to contianers.
+        /// Currently only handles excel files.
+        /// </summary>
+        /// <param name="wbPath">Path to the excel workbook.
+        /// Must end with fully qualifed file name.</param>
+        public jcPageObjectRepository(string wbPath)
+        {
+            loadWorksheets(wbPath);
+        }
+        /// <summary>
+        /// Get a sheet as a list of lists form the stored page object store.
+        /// </summary>
+        /// <param name="pageHandle">Name of the page to get.
+        ///  default is addresses.
+        /// For an excel worksheet it is the tab sheet name.</param>
+        /// <returns></returns>
+        public List<List<string>> GetAddressInfo(String addressType = "addresses")
+        {
+            return getPageInfo(addressType);
+        }
+        /// <summary>
+        /// Gets a sheet from the page object store as a list of lists.
+        /// </summary>
+        /// <param name="pageHandle">Sheet name to get.</param>
+        /// <returns></returns>
+        public List<List<string>> GetPageObjectInfo(String pageHandle)
+        {
+            return getPageInfo(pageHandle);
+        }
+        /// <summary>
+        /// Gets a sheet name form the stored workbook info. If sheet is not found, returns null.
+        /// </summary>
+        /// <param name="pageHandle"></param>
+        /// <returns></returns>
+        private List<List<string>> getPageInfo(string pageHandle)
+        {
+            if (!_pagesInfo.ContainsKey(pageHandle.ToLower()))
+            {
+                Console.WriteLine(String.Format("Worksheet {0} not found, returning empty list", pageHandle));
+                return null;
+            }
+            return _pagesInfo[pageHandle.ToLower()];
+        }
+        /// <summary>
+        /// Makes the call to load the file into memory.
+        /// </summary>
+        /// <param name="wbPath">Path to where workbook is stored. Must end in fully qualified
+        /// file name.</param>
+        private void loadWorksheets(string wbPath)
+        {
+            var type = wbPath.Split('.').LastOrDefault();
+
+            switch (type.ToLower())
+            {
+                case "xlsx":
+                    loadFromExcel(wbPath);
+                    break;
+                default:
+                    throw new Exception(String.Format("Cannot identify file type from filename {0}",
+                        wbPath));
+            }
+
+        }
+        /// <summary>
+        /// Calls the reader to load an excel workbook, and stores the results locally.
+        /// </summary>
+        /// <param name="wbPath">Path to the workbook.</param>
+        private void loadFromExcel(String wbPath)
+        {
+            _pagesInfo = new jcExcelSiteInfoReader(String.Format(wbPath)).GetWorksheets();
+        }
+    }
+}
+
+
+
